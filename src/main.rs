acc3a6c73b7a233b7e9c06690ac94681843fcf99
Kris Warner <kdwarn@protonmail.com>
2023-10-04 20:23:44 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use calamine::{open_workbook, DataType, Range, Reader, Xlsx};
+
+fn main() {
+    println!("Hello, world!");
+    // crawl specific directory, printing filenames
+
+    // open up each file
+
+    // access and print something with calamine (spreadsheet viewer)
+
+    // pull data from each file
+
+    // connect to Oracle
+
+    // delete any data matching what we're about to enter (by date/station)
+
+    // insert data into BIKEPED_TEST database
+
+    // determine notification/confirmation system
+}
